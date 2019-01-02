683b3b6ba7d31484505607528a89c95cd09de7b2
Juan <iktan.lol@gmail.om>
2019-01-01 22:02:39 -0600

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,26 @@
  */
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+/**
+ * ============
+ * ENTORNO
+ * =========
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+/*
+===============
+Base de datos
+===============
+*/
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb://cafeUser:123456ABC@ds147344.mlab.com:47344/cafe'
+}
+
+process.env.URLDB = urlDB;
